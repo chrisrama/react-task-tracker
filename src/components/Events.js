c45ee84acbc76393a61ae73af0c56a506b596d52
Chris Rama <62946196+chrisrama@users.noreply.github.com>
2021-04-29 17:07:00 +0200

    added a name

--- src/components/Events.js
+++ src/components/Events.js
@@ -9,7 +9,7 @@ class HandleClick extends React.Component {
     render(){
         return (
           <div>
-          <h1>Christian </h1>
+          <h1>Christian Ramazani </h1>
           <br/>
           <img onMouseOver={handlclick} src="https://www.fillmurray.com/200/100" alt="nothing" />
  
